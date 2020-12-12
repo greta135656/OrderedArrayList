b992cbe24be4afa476d0b0cde1832867d785fe7f
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-12 00:22:40 +0000

    added constructor

---
+++ OrderedArrayList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>
+public OrderedArrayList(){
+super();
+}
+
+public OrderedArrayList(int startingCapacity){
+super(startingCapacity);
+}
