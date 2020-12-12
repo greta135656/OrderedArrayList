b7f28c981c02fbf8f364ec0f9482d446c1783468
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-12 19:00:53 +0000

    didn't compile

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>
+OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
 public OrderedArrayList(){
 super();
 }
@@ -7,3 +7,22 @@ super();
 public OrderedArrayList(int startingCapacity){
 super(startingCapacity);
 }
+
+
+public void add(int index, T value){
+add(value);
+}
+
+public boolean add(T value){
+for (int i = 0; i < size(); i ++)
+if(get(i).compareTo(value) < 0){
+super.add(i, value);
+}
+return true;
+}
+
+public T set(int index, T value){
+remove(index);
+add(value);
+return value;
+}}
