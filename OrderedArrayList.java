8b9fcd3c84d602b1eb0ee740a2de55bdf7284945
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-13 22:39:27 +0000

    everything should be right I guess

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -14,16 +14,23 @@ add(value);
 }
 
 public boolean add(T value){
+if(value == null){
+super.add(value);
+}
 for (int i = 0; i < size(); i++)
 if(value.compareTo(get(i)) < 0){
-add(i, value);
+super.add(i, value);
+return true;
 }
-
+super.add(value);
 return true;
 }
 
 public T set(int index, T value){
+if(value == null)
+throw new IllegalArgumentException();
+T result = get(index);
 remove(index);
 add(value);
-return value;
+return result;
 }}
